5fbd948a8f372ebc60441744c83fe6c904339078
Lucas Lorentz <lucaslorentzlara@hotmail.com>
2012-12-04 02:43:56 -0200

    First commit

---
+++ knockout.mapper.js
@@ -0,0 +1,226 @@
+(function(factory) {
+    if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
+        // CommonJS or Node: hard-coded dependency on "knockout"
+        factory(require("knockout"), exports);
+    } else if (typeof define === "function" && define["amd"]) {
+        // AMD anonymous module with hard-coded dependency on "knockout"
+        define(["knockout", "exports"], factory);
+    } else {
+        // <script> tag: use the global `ko` object, attaching a `mapping` property
+        factory(ko, ko.mapper = {});
+    }
+})(function(ko, exports) {
+    exports.fromJS = function(value, options, target, wrap) {
+        if (!options) options = {};
+
+        var handler = options.$handler || options;
+        if (typeof(handler) == 'function') handler = handler(value, options, target, wrap);
+        if (typeof(handler) != 'string') handler = 'auto';
+        return exports.handlers[handler].fromJS(value, options, target, wrap);
+    };
+
+    exports.toJS = function(value, options) {
+        if (!options) options = {};
+
+        var handler = options.$handler || options;
+        if (typeof(handler) == 'function') handler = handler(value, options);
+        if (typeof(handler) != 'string') handler = 'auto';
+        return exports.handlers[handler].toJS(value, options);
+    };
+
+    exports.fromJSON = function(value, options, target, wrap) {
+        return exports.fromJS(ko.utils.parseJson(value), options, target, wrap);
+    };
+
+    exports.toJSON = function(value, options) {
+        return ko.utils.stringifyJson(exports.toJS(value, options));
+    };
+
+    exports.handlers = {};
+
+    exports.handlers.auto = {
+        fromJS: function(value, options, target, wrap) {
+            var handler;
+            if (ko.isComputed(target) && !target.hasWriteFunction) {
+                handler = 'ignore';
+            } else {
+                var type = getType(value);
+                if (type == "array") {
+                    handler = 'array';
+                } else if (type == "object") {
+                    handler = 'object';
+                } else {
+                    handler = 'value';
+                }
+            }
+            return exports.handlers[handler].fromJS(value, options, target, wrap);
+        },
+        toJS: function(observable, options) {
+            var value = ko.utils.unwrapObservable(observable);
+            var type = getType(value);
+            var handler;
+            if (type == "array") {
+                handler = 'array';
+            } else if (type == "object") {
+                handler = 'object';
+            } else {
+                handler = 'value';
+            }
+            return exports.handlers[handler].toJS(observable, options);
+        }
+    };
+
+    exports.ignore = {};
+
+    exports.handlers.ignore = {
+        fromJS: function(value, options, target, wrap) {
+            return exports.ignore;
+        },
+        toJS: function(observable, options) {
+            return exports.ignore;
+        }
+    };
+
+    exports.handlers.copy = {
+        fromJS: function(value, options, target, wrap) {
+            return value;
+        },
+        toJS: function(value, options) {
+            return value;
+        }
+    };
+
+    exports.handlers.value = {
+        fromJS: function(value, options, target, wrap) {
+            if (ko.isObservable(target)) {
+                target(value);
+                return target;
+            } else if (wrap) {
+                return ko.observable(value);
+            } else {
+                return value;
+            }
+        },
+        toJS: function(observable, options) {
+            return ko.utils.unwrapObservable(observable);
+        }
+    };
+    exports.handlers.array = {
+        fromJS: function(value, options, target, wrap) {
+            var targetArray = ko.utils.unwrapObservable(target);
+
+            var array;
+
+            if (options.$merge) {
+                array = targetArray;
+                for (var i = 0; i < value.length; i++) {
+                    var item = options.$key && targetArray ? find(targetArray, options.$key, value[i]) : null;
+
+                    var itemOptions = options.$itemOptions;
+                    if (typeof itemOptions == 'function') itemOptions = itemOptions(value, options, target);
+
+                    var val = exports.fromJS(value[i], itemOptions, item, false);
+                    if (val !== exports.ignore && !item) {
+                        array.push(val);
+                    }
+                }
+            } else {
+                array = [];
+                for (var i = 0; i < value.length; i++) {
+                    var item = options.$key && targetArray ? find(targetArray, options.$key, value[i]) : null;
+
+                    var itemOptions = options.$itemOptions;
+                    if (typeof itemOptions == 'function') itemOptions = itemOptions(value, options, target);
+
+                    var val = exports.fromJS(value[i], itemOptions, item, false);
+                    if (val !== exports.ignore) {
+                        array.push(val);
+                    }
+                }
+            }
+            if (ko.isObservable(target)) {
+                target(array);
+                return target;
+            } else if (wrap) {
+                return ko.observableArray(array);
+            } else {
+                return array;
+            }
+        },
+        toJS: function(observable, options) {
+            var value = ko.utils.unwrapObservable(observable);
+            var arr = [];
+            for (var i = 0; i < value.length; i++) {
+                var val = exports.toJS(value[i], options.$itemOptions);
+                if (val !== exports.ignore) {
+                    arr.push(val);
+                }
+            }
+            return arr;
+        }
+    };
+    exports.handlers.object = {
+        fromJS: function(value, options, target, wrap) {
+            var obj = ko.utils.unwrapObservable(target);
+
+            if (!obj) {
+                if (options.$type) obj = new options.$type;
+                else if (options.$create) obj = options.$create(value, options);
+                else obj = {};
+            }
+            for (var p in value) {
+                var val = exports.fromJS(value[p], options[p] || options.$default, obj[p], true);
+                if (val !== exports.ignore && obj[p] != val) {
+                    obj[p] = val;
+                }
+            }
+            if (ko.isObservable(target)) {
+                target(obj);
+                return target;
+            } else if (wrap) {
+                return ko.observable(obj);
+            } else {
+                return obj;
+            }
+        },
+        toJS: function(observable, options) {
+            var value = ko.utils.unwrapObservable(observable);
+            var obj = {};
+            for (var p in value) {
+                var val = exports.toJS(value[p], options[p] || options.$default);
+                if (val !== exports.ignore) {
+                    obj[p] = val;
+                }
+            }
+            return obj;
+        }
+    };
+
+    function find(array, key, data) {
+        if (typeof key === 'function') {
+            var value = key(data);
+            for (var i = 0; i < array.length; i++) {
+                var itemValue = key(array[i]);
+                if (itemValue == value) {
+                    return array[i];
+                }
+            }
+        } else {
+            var value = data[key];
+            for (var i = 0; i < array.length; i++) {
+                var itemValue = ko.utils.unwrapObservable(array[i][key]);
+                if (itemValue == value) {
+                    return array[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    function getType(x) {
+        if (x == null) return null;
+        if (x instanceof Array) return "array";
+        if (x instanceof Date) return "date";
+        return typeof x;
+    }
+});
